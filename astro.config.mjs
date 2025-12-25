e0a7a9336f5bcf5fb08411b94e3522f644c92667
roemdev <emivirdisla@gmail.com>
2025-12-25 19:07:14 -0400

    rebuild

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
+// https://astro.build/config/
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
